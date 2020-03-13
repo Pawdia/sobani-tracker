2392e1e83e97c2b4ca89a077e561397faa72e49b
Ayaka Neko <neko@ayaka.moe>
2020-03-13 14:31:40 +0800

    Optimized

--- test/test_client.js
+++ test/test_client.js
@@ -3,20 +3,20 @@ const axios = require("axios")
 const config = require("../config.json")
 
 // Announce
-let test1Multiaddr = hash.sha256("test1")
-let test2Multiaddr = hash.sha256("test2")
+let test1Multiaddr = "ip4/123.112.121.221/tcp/4000/p2p/" + hash.sha256("test1")
+let test2Multiaddr = "ip4/123.112.121.222/tcp/4001/p2p/" + hash.sha256("test2")
 
 Promise.all([
     axios.post("http://127.0.0.1:" + config.port + "/announce", 
     { 
-        multiaddr: "ip4/123.112.121.221/tcp/4000/p2p/" + test1Multiaddr, 
+        multiaddr: test1Multiaddr, 
         ip: "123.112.121.221", 
         port: "4000"
     }
     ),
     axios.post("http://127.0.0.1:" + config.port + "/announce",
     {
-        multiaddr: "ip4/123.112.121.222/tcp/4001/p2p/" + test2Multiaddr,
+        multiaddr: test2Multiaddr,
         ip: "123.112.121.222",
         port: "4001"
     }
@@ -30,7 +30,7 @@ Promise.all([
     axios.post("http://127.0.0.1:" + config.port + "/push", 
     {
         shareId: test1ShareId,
-        multiaddr: "ip4/123.112.121.222/tcp/4001/p2p/" + test2Multiaddr
+        multiaddr: test2Multiaddr
     }
     ).then(res => {
         console.log(res)
