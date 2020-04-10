e2dd45491884613cf8db1032aedf20b25a11ea44
Cocoa <0xbbc@0xbbc.com>
2020-04-10 22:55:31 +0800

    Rewrite test code
    Signed-off-by: Cocoa <0xbbc@0xbbc.com>

--- test/test_server.js
+++
@@ -1,7 +0,0 @@
-const config = require("../config.json")
-let router = require("../src/router")
-const SobaniServer = require("../src/core/server")
-const app = new SobaniServer()
-
-app.use(router)
-app.listen(config.port)
